c01ff93c5f772eabdfb5378e6b2910fcf9d73569
Yuuki Nishiyama <tetujin@ht.sfc.keio.ac.jp>
2019-08-31 00:30:25 +0900

    Initial Commit

---
+++ HWDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWDetector
+//
+//  Created by Yuuki Nishiyama on 2019/08/31.
+//  Copyright © 2019 Yuuki Nishiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
